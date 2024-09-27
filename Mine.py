4e9cdadfcc19d6972f4759937bb0f3730523977b
Gui <cuervoarangoguillermo@gmail.com>
2024-09-27 10:33:46 +0200

    Commits

--- Mine.py
+++ Mine.py
@@ -1,8 +1,13 @@
 import pygame
+import random
 
 pygame.init()
 
-WIDTH, HEIGHT = 600, 600
+pygame.font.init()
+
+font = pygame.font.SysFont(None, 30)
+
+WIDTH, HEIGHT = 1300, 700
 grid_x = 30
 grid_y = 30
 tile_x = 16
@@ -12,51 +17,42 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("CS IA")
 
-grid = [[tile() for x in range(tile_x)] for y in range(tile_y)]
+possible_tiles = []
 
 class tile:
+    global possible_tiles
     def __init__(self, file):
         self.file = file
         self.right = []
         self.left = []
         self.bottom = []
         self.top = []
+        self.possibilities = []
+        if file not in possible_tiles:
+         possible_tiles.append(file)
 
-    def right(self, neibour):
+    def add_right(self, neibour):
         if(type(neibour) == list):
             self.right.extend(neibour)
         else:
             self.right.append(neibour)
-    def left(self, neibour):
+    def add_left(self, neibour):
         if(type(neibour) == list):
             self.left.extend(neibour)
         else:
             self.left.append(neibour)
-    def bottom(self, neibour):
+    def add_bottom(self, neibour):
         if(type(neibour) == list):
             self.bottom.extend(neibour)
         else:
             self.bottom.append(neibour)
-    def top(self, neibour):
+    def add_top(self, neibour):
         if(type(neibour) == list):
             self.top.extend(neibour)
         else:
             self.top.append(neibour)
 
-def draw_grid(grid):
-    for col in range(grid_y):
-        for row in range(grid_x):
-            tile = grid[col][row]
-            if tile:
-                image = pygame.image.load(tile.file)
-                image = pygame.transform.scale(image, (tile_x,tile_y))
-                screen.blit(image, (col*tile_y, row*tile_y))
-
-    for x in range(0, WIDTH, grid_x):
-        pygame.draw.line(screen, (255,255,255), (x, 0), (x, HEIGHT))
-    for y in range(0, HEIGHT, grid_y):
-        pygame.draw.line(screen, (255,255,255), (0, y), (WIDTH, y))
-
+grid = [[None for x in range(grid_x)] for y in range(grid_y)]
 
 grass = tile("Sprites/Grass")
 horizontal = tile("Sprites/Horizontal_Fence")
@@ -66,37 +62,108 @@ tr = tile("Sprites/Top_Right_Fence")
 bl = tile("Sprites/Bottom_Left_Fence")
 br = tile("Sprites/Bottom_Right_Fence")
 
-horizontal.left([tl,bl])
-horizontal.right([tr,br])
-horizontal.top([grass,horizontal])
-horizontal.bottom([grass,horizontal])
+horizontal.add_left([tl,bl])
+horizontal.add_right([tr,br])
+horizontal.add_top([grass,horizontal])
+horizontal.add_bottom([grass,horizontal])
 
-vertical.left([grass,vertical])
-vertical.right([grass,vertical])
-vertical.top([tl,tr])
-vertical.bottom([bl,br])
+vertical.add_left([grass,vertical])
+vertical.add_right([grass,vertical])
+vertical.add_top([tl,tr])
+vertical.add_bottom([bl,br])
 
-tl.left([grass,vertical])
-tl.right([horizontal,tr,br])
-tl.top([grass,horizontal])
-tl.bottom([vertical, bl,br])
+tl.add_left([grass,vertical])
+tl.add_right([horizontal,tr,br])
+tl.add_top([grass,horizontal])
+tl.add_bottom([vertical, bl,br])
 
-tr.left([tl,horizontal,br])
-tr.right([horizontal,tr])
-tr.top([grass,horizontal])
-tr.bottom([vertical, bl,br])
+tr.add_left([tl,horizontal,br])
+tr.add_right([horizontal,tr])
+tr.add_top([grass,horizontal])
+tr.add_bottom([vertical, bl,br])
 
-bl.left([grass,vertical])
-bl.right([horizontal,tl,tr])
-bl.top([vertical,tl,bl])
-bl.bottom([grass,vertical])
+bl.add_left([grass,vertical])
+bl.add_right([horizontal,tl,tr])
+bl.add_top([vertical,tl,bl])
+bl.add_bottom([grass,vertical])
 
-br.left([horizontal,tl,tr])
-br.right([grass,vertical])
-br.top([vertical,tr,br])
-br.bottom([grass,vertical])
+br.add_left([horizontal,tl,tr])
+br.add_right([grass,vertical])
+br.add_top([vertical,tr,br])
+br.add_bottom([grass,vertical])
 
-grass.left([grass,vertical,tl,bl])
-grass.right([grass,vertical,tr,br])
-grass.top([grass,horizontal,tl,tr])
-grass.bottom([grass,horizontal,bl,br])
+grass.add_left([grass,vertical,tl,bl])
+grass.add_right([grass,vertical,tr,br])
+grass.add_top([grass,horizontal,tl,tr])
+grass.add_bottom([grass,horizontal,bl,br])
+
+def init_grid():
+    for y in range(grid_y):
+        for x in range(grid_x):
+            grid[y][x] = tile("")
+            grid[y][x].possibilities = possible_tiles
+
+def find_uncollapsed_cell():
+    x_coord = random.randrange(grid_x)
+    y_coord = random.randrange(grid_y)
+    for y in range(grid_y):
+        for x in range(grid_x):
+            if len(grid[y][x].possibilities) < len(grid[y_coord][x_coord].possibilities):
+                x_coord = x
+                y_coord = y
+                
+    print(grid[y_coord][x_coord].possibilities)
+    return [x_coord, y_coord]
+
+def collapse_cell(coords):
+   x=coords[0]
+   y=coords[1]
+   tile = grid[y][x]
+   tile.file = random.choice(tile.possibilities)
+   for i in range(len(grid[y-1][x].possibilities)):
+      if grid[y-1][x].possibilities[i] not in tile.top:
+             grid[y-1][x].possibilities = grid[y-1][x].possibilities.pop(i)
+   for i in range(len(grid[y+1][x].possibilities)):
+      if grid[y+1][x].possibilities[i] not in tile.bottom:
+             grid[y+1][x].possibilities = grid[y+1][x].possibilities.pop(i)
+   for i in range(len(grid[y][x+1].possibilities)):
+      if grid[y][x+1].possibilities[i] not in tile.right:
+             grid[y][x+1].possibilities = grid[y][x+1].possibilities.pop(i)
+   for i in range(len(grid[y][x-1].possibilities)):
+      if grid[y][x-1].possibilities[i] not in tile.left:
+             grid[y][x-1].possibilities = grid[y][x-1].possibilities.pop(i)
+
+def draw_grid(grid):
+  for y in range(grid_y):
+    for x in range(grid_x):
+      tile = grid[y][x]
+      if tile.file == "":
+        img = font.render(str(len(tile.possibilities)), True, (150,150,150))
+        screen.blit(img, (x*tile_x, y*tile_y))
+      else:
+        image = pygame.image.load(tile.file)
+        image = pygame.transform.scale(image, (tile_x,tile_y))
+        screen.blit(image, (x*tile_x, y*tile_y))
+
+  for x in range(grid_x + 1): 
+    pygame.draw.line(screen, (255,255,255), (x*tile_x, 0), (x*tile_x, grid_y*tile_y))
+
+  for y in range(grid_y + 1):
+    pygame.draw.line(screen, (255,255,255), (0, y*tile_y), (grid_x*tile_x, y*tile_y))
+
+init_grid()
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    draw_grid(grid)
+    collapse_cell(find_uncollapsed_cell())
+
+    pygame.display.flip()
+
+pygame.quit()
+
--- main.py
+++ main.py
@@ -1,109 +1,187 @@
 import pygame
 import random
 
-# Initialize pygame
 pygame.init()
 
-# Constants
+# Screen settings
 WIDTH, HEIGHT = 600, 600
-GRID_SIZE = 10
-CELL_SIZE = WIDTH // GRID_SIZE
-
-# Colors
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-GRAY = (169, 169, 169)
-WHITE = (255, 255, 255)
-
-# Rule constraints
-rules = {
-    'B': [['Y']],  
-    'Y': [['G']], 
-    'G': [['GRAY']],
-    'GRAY': []
-}
-
-TILE_TYPES = {
-    'B': BLUE,
-    'Y': YELLOW,
-    'G': GREEN,
-    'GRAY': GRAY
-}
+grid_x = 30
+grid_y = 30
+tile_x = 16
+tile_y = 16
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Wave Function Collapse - Pygame")
+pygame.display.set_caption("CS IA")
 
-def draw_grid(grid):
-    for row in range(GRID_SIZE):
-        for col in range(GRID_SIZE):
-            tile = grid[row][col]
-            if tile:
-                color = TILE_TYPES[tile]
-                pygame.draw.rect(screen, color, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+# Define the Tile class
+class tile:
+    def __init__(self, file):
+        self.file = file
+        self.right = []
+        self.left = []
+        self.bottom = []
+        self.top = []
 
-    for x in range(0, WIDTH, CELL_SIZE):
-        pygame.draw.line(screen, WHITE, (x, 0), (x, HEIGHT))
-    for y in range(0, HEIGHT, CELL_SIZE):
-        pygame.draw.line(screen, WHITE, (0, y), (WIDTH, y))
+    def right(self, neibour):
+        if(type(neibour) == list):
+            self.right.extend(neibour)
+        else:
+            self.right.append(neibour)
+    def left(self, neibour):
+        if(type(neibour) == list):
+            self.left.extend(neibour)
+        else:
+            self.left.append(neibour)
+    def bottom(self, neibour):
+        if(type(neibour) == list):
+            self.bottom.extend(neibour)
+        else:
+            self.bottom.append(neibour)
+    def top(self, neibour):
+        if(type(neibour) == list):
+            self.top.extend(neibour)
+        else:
+            self.top.append(neibour)
 
-def get_valid_neighbors(tile):
-    if tile in rules:
-        return rules[tile]
-    return []
+grass = tile("Sprites/Grass")
+horizontal = tile("Sprites/Horizontal_Fence")
+vertical = tile("Sprites/Vertical_Fence")
+tl = tile("Sprites/Top_Left_Fence")
+tr = tile("Sprites/Top_Right_Fence")
+bl = tile("Sprites/Bottom_Left_Fence")
+br = tile("Sprites/Bottom_Right_Fence")
 
-def get_neighbors(row, col):
-    neighbors = []
-    if row > 0:  # Up
-        neighbors.append((row - 1, col))
-    if col < GRID_SIZE - 1:  # Right
-        neighbors.append((row, col + 1))
-    if row < GRID_SIZE - 1:  # Down
-        neighbors.append((row + 1, col))
-    if col > 0:  # Left
-        neighbors.append((row, col - 1))
-    return neighbors
+horizontal.left([tl,bl])
+horizontal.right([tr,br])
+horizontal.top([grass,horizontal])
+horizontal.bottom([grass,horizontal])
+
+vertical.left([grass,vertical])
+vertical.right([grass,vertical])
+vertical.top([tl,tr])
+vertical.bottom([bl,br])
+
+tl.left([grass,vertical])
+tl.right([horizontal,tr,br])
+tl.top([grass,horizontal])
+tl.bottom([vertical, bl,br])
+
+tr.left([tl,horizontal,br])
+tr.right([horizontal,tr])
+tr.top([grass,horizontal])
+tr.bottom([vertical, bl,br])
+
+bl.left([grass,vertical])
+bl.right([horizontal,tl,tr])
+bl.top([vertical,tl,bl])
+bl.bottom([grass,vertical])
+
+br.left([horizontal,tl,tr])
+br.right([grass,vertical])
+br.top([vertical,tr,br])
+br.bottom([grass,vertical])
+
+grass.left([grass,vertical,tl,bl])
+grass.right([grass,vertical,tr,br])
+grass.top([grass,horizontal,tl,tr])
+grass.bottom([grass,horizontal,bl,br])
+
+# List of all tile types
+all_tiles = [grass, horizontal, vertical, tl, tr, bl, br]
+
+# WFC Algorithm (simplified)
+class WFC:
+    def __init__(self, grid_width, grid_height, tiles):
+        self.grid_width = grid_width
+        self.grid_height = grid_height
+        self.tiles = tiles
+        self.grid = [[self.initialize_tile_options() for _ in range(grid_height)] for _ in range(grid_width)]
+
+    def initialize_tile_options(self):
+        """Each grid cell starts with all possible tiles (uncollapsed)."""
+        return list(self.tiles)
+
+    def collapse_tile(self, x, y):
+        """Collapse a single tile at grid[x][y]."""
+        possible_tiles = self.grid[x][y]
+
+        # Randomly select a tile if it's not collapsed yet
+        if len(possible_tiles) > 1:
+            selected_tile = random.choice(possible_tiles)
+            self.grid[x][y] = [selected_tile]  # Collapse to this tile
+            self.propagate_constraints(x, y, selected_tile)
+
+    def propagate_constraints(self, x, y, tile):
+        """Propagate constraints to neighboring tiles after collapsing a tile."""
+        # Check and update the right neighbor
+        if x < self.grid_width - 1:
+            self.update_neighbors(x + 1, y, tile.right)
+
+        # Check and update the left neighbor
+        if x > 0:
+            self.update_neighbors(x - 1, y, tile.left)
+
+        # Check and update the top neighbor
+        if y > 0:
+            self.update_neighbors(x, y - 1, tile.top)
+
+        # Check and update the bottom neighbor
+        if y < self.grid_height - 1:
+            self.update_neighbors(x, y + 1, tile.bottom)
+
+    def update_neighbors(self, x, y, allowed_neighbors):
+        """Reduce the tile options for a neighboring cell based on allowed neighbors."""
+        current_options = self.grid[x][y]
+        self.grid[x][y] = [tile for tile in current_options if tile in allowed_neighbors]
+
+    def is_fully_collapsed(self):
+        """Check if all grid cells have been collapsed."""
+        return all(len(self.grid[x][y]) == 1 for x in range(self.grid_width) for y in range(self.grid_height))
+
+    def collapse(self):
+        """Main loop to collapse the entire grid step by step."""
+        while not self.is_fully_collapsed():
+            # Find a random cell to collapse
+            x, y = self.find_uncollapsed_cell()
+            if x is None:
+                break
+
+            # Collapse the tile at this cell
+            self.collapse_tile(x, y)
+
+    def find_uncollapsed_cell(self):
+        """Find a random uncollapsed cell in the grid."""
+        uncollapsed_cells = [(x, y) for x in range(self.grid_width) for y in range(self.grid_height) if len(self.grid[x][y]) > 1]
+        return random.choice(uncollapsed_cells) if uncollapsed_cells else (None, None)
+
+    def draw(self):
+        """Draw the grid."""
+        for x in range(self.grid_width):
+            for y in range(self.grid_height):
+                if len(self.grid[x][y]) == 1:  # If collapsed, draw the tile
+                    tile = self.grid[x][y][0]
+                    image = pygame.image.load(tile.file)
+                    image = pygame.transform.scale(image, (tile_x, tile_y))
+                    screen.blit(image, (x * tile_x, y * tile_y))
 
 
-def collapse(grid, possibilities, row, col):
-    chosen_tile = random.choice(possibilities[row][col])
-    grid[row][col] = chosen_tile
-    possibilities[row][col] = [chosen_tile]
+# Initialize the WFC grid
+wfc = WFC(grid_x, grid_y, all_tiles)
 
-    neighbors = get_neighbors(row, col)
-    for n_row, n_col in neighbors:
-        if grid[n_row][n_col] is None:
-            valid_neighbors = get_valid_neighbors(chosen_tile)
-            possibilities[n_row][n_col] = [tile for tile in possibilities[n_row][n_col] if tile in valid_neighbors]
+# Main game loop
+running = True
+while running:
+    screen.fill((0, 0, 0))
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-def wave_function_collapse():
-    # Create a grid and initialize all cells as empty (None)
-    grid = [[None for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+    # Collapse the grid and draw it
+    wfc.collapse()
+    wfc.draw()
 
-    possibilities = [[list(TILE_TYPES.keys()) for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+    pygame.display.flip()
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+pygame.quit()
 
-        uncollapsed_cells = [(r, c) for r in range(GRID_SIZE) for c in range(GRID_SIZE) if grid[r][c] is None]
-        if uncollapsed_cells:
-
-            row, col = random.choice(uncollapsed_cells)
-            collapse(grid, possibilities, row, col)
-
-        screen.fill((0, 0, 0))
-
-        draw_grid(grid)
-
-        pygame.display.flip()
-
-        pygame.time.delay(100)
-
-    pygame.quit()
-
-if __name__ == "__main__":
-    wave_function_collapse()
